492e3e05a37b0f836aab008903ee45a335638512
smoothflowcompliance <smoothflowcompliance@gmail.com>
2025-12-25 21:26:24 +0530

    invoice menu

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Button } from './components/ui/button'
+import LandingPage from './pages/landingPage/LandingPage'
+import Dashboard from './pages/Dashboard'
+import MainPage from './pages/MainPage'
+import PreviewPage from './pages/PreviewPage'
+import Menubar from './components/Menubar'
+import { Toaster } from 'react-hot-toast'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Menubar/>
+        <Toaster/>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/generate' element={<MainPage/>}/>
+          <Route path='preview' element={<PreviewPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Menubar.jsx
@@ -0,0 +1,30 @@
+import { assets } from '@/assets/assets'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from './ui/button'
+
+const Menubar = () => {
+    return (
+        <div>
+            <div className=' flex justify-around items-center border-b-2 border-b-gray-300 h-20'>
+                <div>
+                    <Link to='/' className=' flex items-center gap-2'>
+                        <img src={assets.logo} className=' w-15 h-15' />
+                        <span className=' font-bold text-orange-400'>QuickInvoice</span>
+                    </Link>
+                </div>
+                <div className=' md:flex gap-8 items-center hidden font-semibold text-lg'>
+                    <Link to='/'>Home</Link>
+                    <Link to='/dashboard'>Dashboard</Link>
+                    <Link to='/generate'>Generate</Link>
+                    <div className=''>
+                        <Button className=' bg-sky-600 text-white cursor-pointer text-sm'>Login/Signup</Button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Menubar
